eb5b92a16befc35c2c3d107691f5d365ac8ae36d
g3scorner <163410336+g3scorner@users.noreply.github.com>
2024-03-15 16:19:44 +0000

    Initial commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,20 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+  // Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/when-will-funding-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var whenWillFunding = req.session.data['when-will-funding']
+
+  // Check whether the variable matches a condition
+  if (whenWillFunding == "An event or purchase in the future"){
+    // Send user to next page
+    res.redirect('/provide-details')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+})
